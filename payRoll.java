2169ed9e1c8cfde2040fde776e376a346fe355dc
nyarkoemmanuel <115224021+nyarkoemmanuel@users.noreply.github.com>
2022-10-26 01:25:00 +0000

    Add files via upload

---
+++ payRoll.java
@@ -0,0 +1,41 @@
+public class payRoll {
+public static void  main(String[] args){
+    String myRegularWages= "Regular Wages:";
+    String myBasepay= "basePay"= 25;";
+    Strings my Regular= "Regular hours = 40";
+    float myOvertime= 37.5f;
+    int myOverPay= 35;
+    int myOverHours=10;
+    float myTotalWages= Regular + myOvertime;
+
+
+
+    System.out.println(myRegularWages +" "+ Regular);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
